ea9e4f3c15ebe95e90883d30782ea0c5220bfc13
The Body4 <35223548+mrkrein@users.noreply.github.com>
2022-02-14 21:30:06 +0300

    Update b4tech_copy.js

--- b4tech_copy.js
+++ b4tech_copy.js
@@ -1,6 +1,6 @@
 let d = new Date();
 let ver = "1.0.1";
-var text = "<!-- Данный сайт был написан: B4Tech P.S Витала пидор -->" + "<!-- INST: @body4_drift VK: body4_drift -->" + "<!-- Текущая дата, полученная с сервера " + d + "-->" + "<!-- Версия скрипта автообновления " + ver + "-->";
+var text = "<!-- Данный сайт был написан: B4Tech P.S -->" + "<!-- INST: @body4_drift VK: body4_drift -->" + "<!-- Текущая дата, полученная с сервера " + d + "-->" + "<!-- Версия скрипта автообновления " + ver + "-->";
 
 var body = document.getElementsByTagName('body');
 if (body != null) {
