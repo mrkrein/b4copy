43a3c9eef44c73416601458da9359833e2c81d70
The Body4 <35223548+mrkrein@users.noreply.github.com>
2022-02-14 21:16:23 +0300

    Add files via upload

---
+++ b4tech_copy.js
@@ -0,0 +1,10 @@
+let d = new Date();
+let ver = "1.0.1";
+var text = "<!-- Данный сайт был написан: B4Tech -->" + "<!-- INST: @body4_drift VK: body4_drift -->" + "<!-- Текущая дата, полученная с сервера " + d + "-->" + "<!-- Версия скрипта автообновления " + ver + "-->";
+
+var body = document.getElementsByTagName('body');
+if (body != null) {
+    body = body[0];
+    body.insertAdjacentHTML('beforebegin', text);
+}
+
