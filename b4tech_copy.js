c4a0edc902295acf6c35087225f06b0bc6f342ec
The Body4 <35223548+mrkrein@users.noreply.github.com>
2022-02-15 04:22:17 +0300

    Update b4tech_copy.js

--- b4tech_copy.js
+++ b4tech_copy.js
@@ -1,10 +1,36 @@
 let d = new Date();
-let ver = "1.0.1";
-var text = "<!-- Данный сайт был написан: B4Tech P.S -->" + "<!-- INST: @body4_drift VK: body4_drift -->" + "<!-- Текущая дата, полученная с сервера " + d + "-->" + "<!-- Версия скрипта автообновления " + ver + "-->";
+let ver = "2.1.1";
+var pass = "1234qwerty"
+var text = "<!-- Данный сайт был написан: B4Tech -->" + "<!-- INST: @body4_drift VK: body4_drift -->" + "<!-- Текущая дата, полученная с сервера " + d + "-->" + "<!-- Версия скрипта автообновления " + ver + "-->";
+var jqState = null;
+LockDown();
 
-var body = document.getElementsByTagName('body');
-if (body != null) {
-    body = body[0];
-    body.insertAdjacentHTML('beforebegin', text);
+/* Подключаем JQuery */
+async function jqActive() {
+    var jqScript = document.createElement('script');
+    jqScript.src = "https://code.jquery.com/jquery-3.6.0.min.js";jqScript.integrity = "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=";jqScript.crossOrigin="anonymous";jqScript.id="jqScript";jqScript.async=true;
+    document.getElementsByTagName('head')[0].appendChild(jqScript);
+    jqScript.onload = function() {
+        InfoScript();
+    }
 }
 
+function InfoScript() {
+    var IScript = document.createElement('script');
+    IScript.src = "info.js";IScript.defer = true;
+    document.getElementsByTagName('body')[0].appendChild(IScript);
+}
+
+function LockDown() {
+    var password = prompt('Введите код разблокировки!');
+    if (new RegExp(pass.replace(/(\.|\+|\[|\]|\(|\)|\{|\}|\*|\?|\$|\^|\\|\/|\.)/g, '\\$1')).test(password)) alert('Код верный!'), LockStatus="disabled"; else alert('НЕВЕРНО'), window.stop(), top.location.href = '/';
+}
+
+    
+    
+ 
+
+    
+
+jqActive();
+        
